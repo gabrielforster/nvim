fe2eedc936d843996ef6a0a7a27aa1908dda36a3
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-06-07 18:12:58 -0300

    feat: return to old theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,6 @@
 function SetColors(color)
  -- moonfly, darkplus, catppuccin, github, github_dark, github_dark_dimmed, github_dark_high_contrast
-	color = color or "github_dark_high_contrast"
+	color = color or "moonfly"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
