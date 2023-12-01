00752d8e911e54b2a8b2005a35565add61439188
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-12-01 15:07:53 -0300

    feat: transparent param on SetColors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,11 +1,16 @@
-function SetColors(color)
+function SetColors(color, transparent)
 	-- color = color or "rose-pine"
 	color = color or "gruvbox"
 	-- color = color or "slate"
 	-- color = color or "habamax"
 	vim.cmd.colorscheme(color)
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  transparent = transparent or false
+
+  if transparent then
+   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  end
+
 end
 SetColors()
