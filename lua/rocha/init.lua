84169e17b77c2fd3275b49e25b527f4ca524d4ad
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-12-26 07:58:15 -0300

    feat: lua line, highlight search and show hidden files on telescope

--- lua/rocha/init.lua
+++ lua/rocha/init.lua
@@ -1,3 +1,4 @@
 require("rocha.remap")
 require("rocha.set")
 require("rocha.icons")
+require("rocha.telescope")
--- lua/rocha/packer.lua
+++ lua/rocha/packer.lua
@@ -1,16 +1,12 @@
-
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim' -- Package manager
+
+  use 'romainl/vim-cool' -- Highlight only current search
 
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
@@ -18,12 +14,8 @@ return require('packer').startup(function(use)
     'lewis6991/gitsigns.nvim',
   }
 
-  -- Colorschemes
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
-  use('morhetz/gruvbox')
-
-	use ( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use("nvim-treesitter/nvim-treesitter-context", {run = ':TSUpdate'});
+	use ( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use("nvim-treesitter/nvim-treesitter-context", { run = ':TSUpdate' });
 
 	use {
 		'VonHeikemen/lsp-zero.nvim',
@@ -48,24 +40,44 @@ return require('packer').startup(function(use)
 		}
 	}
 
+  -- Auto pairs and auto tags
   use {
     "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
   }
-
   use {
     "windwp/nvim-ts-autotag",
     config = function() require('nvim-ts-autotag').setup() end
   }
 
-  use 'nvim-tree/nvim-web-devicons'
-
   use 'ThePrimeagen/vim-be-good'
 
+  -- Makes easy to comment and uncomment code
   use {
     'numToStr/Comment.nvim',
     config = function()
         require('Comment').setup()
     end
   }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require("lualine").setup {
+        options = {
+          theme = "gruvbox",
+          -- section_separators = { "", "" },
+          -- component_separators = { "", "" },
+          section_separators = { "" },
+          component_separators = { "" },
+          icons_enabled = true,
+        },
+      }
+    end
+  }
+  -- Appearance
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  use 'nvim-tree/nvim-web-devicons'
+  use('morhetz/gruvbox')
+
 end)
