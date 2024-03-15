0ae0bfb85b31fbbc2b0acca7a622cd7f456d414d
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-03-15 08:16:21 -0300

    feat: partial accept on copilot suggestions

--- lua/rocha/init.lua
+++ lua/rocha/init.lua
@@ -51,6 +51,8 @@ autocmd("LspAttach", {
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+
+    vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
   end
 })
 
