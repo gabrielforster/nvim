a83789f354c2a584ba016aecd506ac4b670b9fa5
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-04-02 14:49:15 -0300

    Revert "feat: partial accept on copilot suggestions"
    This reverts commit 0ae0bfb85b31fbbc2b0acca7a622cd7f456d414d.

--- lua/rocha/init.lua
+++ lua/rocha/init.lua
@@ -51,8 +51,6 @@ autocmd("LspAttach", {
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-
-    vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
   end
 })
 
