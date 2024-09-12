57e0f2fa3c8fd73cf467f3877d2a15716117e567
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-09-11 23:41:10 -0300

    changing colors and adding lua line

--- lua/rocha/lazy/colors.lua
+++ lua/rocha/lazy/colors.lua
@@ -1,99 +1,95 @@
 function SetColors(color, bg)
-    color = color or "tokyonight"
-    -- color = color or "atlas"
-    vim.cmd.colorscheme(color)
+  color = color or "tokyonight"
+  -- color = color or "atlas"
+  vim.cmd.colorscheme(color)
 
-    if bg then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
+  if bg then
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  end
 end
 
 function Gruvbox()
-    local color = "gruvbox"
-    vim.cmd.colorscheme(color)
+  local color = "gruvbox"
+  vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-    { "EdenEast/nightfox.nvim" },
-    { "rebelot/kanagawa.nvim" },
-    { "huyvohcmc/atlas.vim" },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require("rose-pine").setup({
-                styles = {
-                    background = false,
-                },
-                disable_background = true,
-            })
+  { "EdenEast/nightfox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "huyvohcmc/atlas.vim" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        styles = {
+          background = false,
+        },
+        disable_background = true,
+      })
+    end
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    config = function()
+      require("gruvbox").setup()
+    end
+  },
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        style = "moon",
+        transparent = true,
+        italics = {
+          comments = true,
+          keywords = true,
+          functions = true,
+          strings = true,
+          variables = true,
+        },
+      })
 
-            vim.cmd("colorscheme rose-pine")
-
-            SetColors()
-        end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup()
-        end
-    },
-    {
-        "folke/tokyonight.nvim",
-        config = function ()
-            require("tokyonight").setup({
-                style = "night",
-                transparent = false,
-                italics = {
-                    comments = true,
-                    keywords = true,
-                    functions = true,
-                    strings = true,
-                    variables = true,
-                },
-            })
-
-            SetColors("tokyonight", true)
-        end,
-        name = "tokyonight",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup({
-                options = {
-                    theme = "tokyonight",
-                    section_separators = { "", "" },
-                    component_separators = { "", "" },
-                },
-                sections = {
-                    lualine_a = { "mode" },
-                    lualine_b = { "branch" },
-                    lualine_c = { "filename" },
-                    lualine_x = { "encoding", "fileformat", "filetype" },
-                    lualine_y = { "progress" },
-                    lualine_z = { "location" },
-                },
-                inactive_sections = {
-                    lualine_a = {},
-                    lualine_b = {},
-                    lualine_c = { "filename" },
-                    lualine_x = { "location" },
-                    lualine_y = {},
-                    lualine_z = {},
-                },
-                extensions = { "nvim-tree" },
-            })
-        end,
-    }
+      SetColors(nil, true)
+    end,
+    name = "tokyonight",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "tokyonight",
+          section_separators = { "", "" },
+          component_separators = { "", "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch" },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        extensions = { "nvim-tree" },
+      })
+    end,
+  }
 }
