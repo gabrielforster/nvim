59f82c0376716e4dcfb2945495a9d990c54ebca2
Gabriel Rocha <rochafrgabriel@gmail.com>
2025-11-01 14:29:09 -0300

    feat: show hidden files on Oil

--- lua/rocha/pluggins/oil.lua
+++ lua/rocha/pluggins/oil.lua
@@ -1,13 +1,13 @@
 return {
   "stevearc/oil.nvim",
-  opts = {
-    view_options = {
-      show_hidden = true,
-    }
-  },
+  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("oil").setup({})
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      },
+    })
     vim.keymap.set({ "n", "x" }, "<leader><tab>", function() require("oil").open() end, { silent = true })
   end
 }
