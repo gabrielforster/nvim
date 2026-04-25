9834814503c5e6d583372a8757720ed92c0415f2
Gabriel Rocha <rochafrgabriel@gmail.com>
2026-04-25 09:30:03 -0300

    comment Oil out

--- lua/rocha/pluggins/oil.lua
+++ lua/rocha/pluggins/oil.lua
@@ -1,13 +1,14 @@
-return {
-  "stevearc/oil.nvim",
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({
-      view_options = {
-        show_hidden = true,
-      },
-    })
-    vim.keymap.set({ "n", "x" }, "<leader><tab>", function() require("oil").open() end, { silent = true })
-  end
-}
+return {}
+-- return {
+--   "stevearc/oil.nvim",
+--   opts = {},
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   config = function()
+--     require("oil").setup({
+--       view_options = {
+--         show_hidden = true,
+--       },
+--     })
+--     vim.keymap.set({ "n", "x" }, "<leader><tab>", function() require("oil").open() end, { silent = true })
+--   end
+-- }
